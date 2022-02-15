329cfab311435a26641af19c5d0fa7516a716b4d
YXR <yangxuru870918@gmail.com>
2022-02-16 00:15:08 +0800

    20220216_shopping cart.

--- routes/ShoppingCartRouter.js
+++ routes/ShoppingCartRouter.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
     app.get("/carts/history/:userId", App.history);
     app.get("/cart/:cartId", App.findOne);
     app.get("/users/:userId/favorite", App.findFavorite);
-    app.get("/bill", App.bill);
+    app.get("/shoppingcart/:id", App.shoppingcart);
     // app.get("/groupmember/:id", App.user);
     app.get("/users/:userId", App.personTotalPay)
     app.delete("/:cartId", App.removeCart);
